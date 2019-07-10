e41256fcd7549812342b4cff5bbbe347f27b41cb
Philipp Lamp <peez80@users.noreply.github.com>
2019-07-10 10:42:29 +0200

    Update start-ssh-tunnel.sh

--- scripts/start-ssh-tunnel.sh
+++ scripts/start-ssh-tunnel.sh
@@ -3,7 +3,19 @@
 
 mkdir -p ~/.ssh
 
+if [ -z "$SSH_PORT" ]; then
+  SSH_PORT=22
+fi
+
+
+if [ -z "$SSH_PROXY_COMMAND" ]; then
+  THE_PROXY_COMMAND= 
+else
+  THE_PROXY_COMMAND="-o ProxyCommand='$PROXY_COMMAND'"
+fi
+
+
 ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
 
 
-ssh -i $ID_FILE -nNT -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
+ssh -i $ID_FILE -p $SSH_PORT -nNT $THE_PROXY_COMMAND -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
