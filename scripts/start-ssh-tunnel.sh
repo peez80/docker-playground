a6bb5d79b4a30f075252936764bb0c5c7bcedc47
Philipp Lamp <philipp.lamp@bmw.de>
2019-11-11 14:56:22 +0100

    change chmod -r to chmod -R because -r suddenly stopped working. How could this work ever?!

--- scripts/start-ssh-tunnel.sh
+++ scripts/start-ssh-tunnel.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # Script is intended to be called via docker cmd. Values are set by Environment parameters
 # Running container must have a valid .ssh directory mounted to /app/ssh
+set -e
 
+
+echo "mkdir"
 mkdir -p ~/.ssh
+echo "cp"
 cp -r /app/ssh/* ~/.ssh
-chmod -r 600 ~/.ssh
+
+echo "ssh-dir:"
+ls -lah ~/.ssh
+
+echo "chmod"
+chmod -R 600 ~/.ssh
 
 if [ -z "$SSH_PORT" ]; then
   SSH_PORT=22
 fi
 
-
+echo "ssh-keyscan"
 ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts
 
-echo "ssh-dir:"
-ls -lah ~/.ssh
+
 
 ssh -p $SSH_PORT -nNT -o GatewayPorts=true -L $LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT $SSH_USER@$SSH_HOST
